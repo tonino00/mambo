1ecfcef08333d36c4e381d1d30fdaa6c5dbb82a5
antonino90 <antoninocgama@gmail.com>
2017-09-20 18:10:10 -0300

    update form

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -23,7 +23,7 @@
 						<input type="hidden" name="token" value="{{ $token }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
+							<label class="col-md-4 control-label">E-Mail</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
 							</div>
